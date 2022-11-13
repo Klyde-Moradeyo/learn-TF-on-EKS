a331e567f440c5a972529fb40a9e667d586f9955
Klyde <moradeyo.klyde@gmail.com>
2022-11-13 23:57:26 +0000

    Fix syntax error in c6-outputs.tf

--- terraform-manifests/c6-outputs.tf
+++ terraform-manifests/c6-outputs.tf
@@ -16,7 +16,7 @@ output "for_output_list" {
 # Output - For loop with map
 output "for_output_map1" {
     description = "For Loop with Map"
-    value = { for instance in aws_instance.myec2vm: instance.id => instance.public_dn }
+    value = { for instance in aws_instance.myec2vm: instance.id => instance.public_dns }
 }
 
 # Output - For Loop with Map Advanced
