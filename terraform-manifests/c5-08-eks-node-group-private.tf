936b7c9e9730febc669db614a93df9a10dbce841
Klyde <moradeyo.klyde@gmail.com>
2023-02-22 13:12:37 +0000

    Remove unnecessary resources for current section

--- terraform-manifests/c5-08-eks-node-group-private.tf
+++ terraform-manifests/c5-08-eks-node-group-private.tf
@@ -1,4 +1,5 @@
 # Create AWS EKS Node Group - Private
+/*
 resource "aws_eks_node_group" "eks_ng_private" {
   cluster_name    = aws_eks_cluster.eks_cluster.name
 
@@ -41,3 +42,4 @@ resource "aws_eks_node_group" "eks_ng_private" {
     Name = "private-Node-Group"
   }
 }
+*/
