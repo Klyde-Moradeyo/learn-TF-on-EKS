3585a700918cabaa7aaac6512955adeaf2dcc3bc
Klyde <moradeyo.klyde@gmail.com>
2023-02-22 12:38:07 +0000

    Modify c4-07-ec2bastion-provsioners

--- terraform-manifests/c4-07-ec2bastion-provisioners.tf
+++ terraform-manifests/c4-07-ec2bastion-provisioners.tf
@@ -1,44 +1,31 @@
 # Create a Null Resource and Provisioners
-#
-# Copy the private-key of my EC2 keypair into 
-# the bastion Ec2 instance
 resource "null_resource" "copy_ec2_keys" {
-    depends_on = [ module.ec2_public ] # null_resource executes after modul ec2_public is ready
+  depends_on = [module.ec2_public]
+  # Connection Block for Provisioners to connect to EC2 Instance
+  connection {
+    type     = "ssh"
+    host     = aws_eip.bastion_eip.public_ip    
+    user     = "ec2-user"
+    password = ""
+    private_key = file("private-key/terraform-key.pem")
+  }  
 
-    # connection block for provisoners to connect to Ec2 Instrance
-    connection {
-        type = "ssh"
-        host = aws_eip.bastion_eip.public_ip
-        user = "ec2-user"
-        password = ""
-        private_key = file("private-key/terraform-key.pem")
-    }
+## File Provisioner: Copies the terraform-key.pem file to /tmp/terraform-key.pem
+  provisioner "file" {
+    source      = "private-key/terraform-key.pem"
+    destination = "/tmp/terraform-key.pem"
+  }
+## Remote Exec Provisioner: Using remote-exec provisioner fix the private key permissions on Bastion Host
+  provisioner "remote-exec" {
+    inline = [
+      "sudo chmod 400 /tmp/terraform-key.pem"
+    ]
+  }
+## Local Exec Provisioner:  local-exec provisioner (Creation-Time Provisioner - Triggered during Create Resource)
+  provisioner "local-exec" {
+    command = "echo VPC created on `date` and VPC ID: ${module.vpc.vpc_id} >> creation-time-vpc-id.txt"
+    working_dir = "local-exec-output-files/"
+    #on_failure = continue
+  }
 
-    # Set the working directory for all provisioners within this null resource
-    # was getting no directory error in line 42
-    provisioner "local-exec" {
-        command = "mkdir -p local-exec-output-files"
-        working_dir = "."
-    }
-
-    # file provisioners: copies the terraform-key.pem file to /tmp/terraform-key.pem
-    provisioner "file" {
-        source = "private-key/terraform-key.pem"
-        destination = "/tmp/terraform-key.pem" # /tmp/ has full access to copy
-    }
-
-    # Remote Exec provisioners fix the private key permissions in bastion host
-    provisioner "remote-exec" {
-        inline = [
-            "sudo chmod 400 /tmp/terraform-key.pem"
-        ]
-    }
-
-    # local exec provisioner: local-exec provisoion (creation-time provisioner)
-    provisioner "local-exec" {
-        # command is going to get the vpc id.
-        command = "echo VPC created on `date` and VPC ID: ${module.vpc.vpc_id} >> creation-time-vpc-id.txt"
-        # working_dir = "local-exec-output-files/"
-        #on_failure = continue
-    }
 }
